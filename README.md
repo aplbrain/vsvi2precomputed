fd5932bf44f0d6d0729aad219c4254990af23e98
Hannah Martinez <hannah.gooden@jhuapl.edu>
2024-05-10 20:02:11 +0000

    Command line option

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # vsvi2precomputeds
 ![Logo](logo.png)
 Package for converting VSVI (used in VAST) image datasets to precomputed volumes.
+
+## Usage
+```
+pip install -r requirements.txt
+python vsvi2precomputed.py -i s3://path/to/info.vsvi -o s3://path/to/output/dir/
+```
+Don't forget the trailing slash on the output dir.
+Currently uses `default` aws profile. Need to add optional command line param.
---
+++ vsvi2precomputed.py
@@ -0,0 +1,31 @@
+import vsvi2precomputed as vp
+import boto3
+import argparse
+import warnings
+import os
+warnings.filterwarnings("ignore")
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+                    prog='vsvi2precomputed',
+                    description='Convert an s3 dataset from vsvi format to precomputed format',
+    )
+    parser.add_argument('-i', '--input_vsvi', help="Path to s3 location of input dataset .vsvi file")
+    parser.add_argument('-o', '--output_dir', help="Path to s3 location for converted output")
+
+    args = parser.parse_args()
+    # TODO: add some validation like for trailing slash on output dir
+
+    precomputed_cloud_path = args.output_dir
+
+    vsvi_root_path, vsvi_filename = os.path.split(args.input_vsvi)
+    vsvi_cloud_path = args.input_vsvi
+
+    vsvi_data = vp.parse_vsvi(vsvi_cloud_path)
+    vp.upload_tiles_to_precomputed(vsvi_root_path, vsvi_data, precomputed_cloud_path)
+    print("Done")
+
+if __name__ == "__main__":
+    main()
